2b9162f2734fb174260a66c8dacf8a5d10f8ca57
Victor Turra <victor.florencio@univem.edu.br>
2021-11-27 22:11:34 -0300

    Create login and chat pages

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import Chat from "../components/Chat";
+import SideBar from "../components/SideBar";
+
+function Dashboard() {
+  const availableChats = [
+    {
+      id: "eb10822d-5e17-44d4-8b1a-da3ee709d77f",
+      first_name: "Amanda",
+      last_name: "Turra",
+      cpf: "12304931802",
+      phone: "14997774244",
+      birth_date: "2002-01-26T00:00:00.000Z",
+      biography: null,
+      info: null,
+      graduation: null,
+      job: null,
+      created_at: "2021-10-13T00:37:57.345Z",
+      updated_at: "2021-10-13T00:37:57.345Z",
+    },
+    {
+      id: "74537778-eb2b-431e-90c3-e348c9a45c5e",
+      first_name: "Gabriel",
+      last_name: "Turra",
+      cpf: "12304931802",
+      phone: "14997774244",
+      birth_date: "2008-05-12T00:00:00.000Z",
+      biography: null,
+      info: null,
+      graduation: null,
+      job: null,
+      created_at: "2021-10-13T00:48:36.705Z",
+      updated_at: "2021-10-13T00:48:36.705Z",
+    },
+  ];
+
+  const contacts = availableChats.map((user) => ({
+    ...user,
+    fullName: `${user.first_name} ${user.last_name}`,
+  }));
+
+  const [socket, setSocket] = useState();
+  const [activeChat, setActiveChat] = useState({
+    index: 0,
+    userId: availableChats[0].id,
+    user: availableChats[0],
+  });
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:3000");
+    setSocket(newSocket);
+  }, []);
+
+  useEffect(() => {
+    // socket.emit("select_contact", {
+    //   loggedUser: 
+    // })
+  }, [activeChat]);
+
+  function handleSideBarChange({ selected }) {
+    console.log(selected)
+    setActiveChat({
+      index: availableChats.findIndex((user) => user.id === selected.id),
+      userId: selected.id,
+      user: selected,
+    });
+  }
+
+  return (
+    <div className="h-100">
+      <div className="d-flex h-100">
+        <div style={{ maxWidth: "400px" }}>
+          <SideBar
+            onChange={handleSideBarChange}
+            contacts={contacts}
+            activeChat={activeChat}
+          />
+        </div>
+        <div className="flex-grow-1">
+          <Chat contact={activeChat} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { useAuth } from "../contexts/auth";
+import { Redirect } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signIn, signed } = useAuth();
+
+  function handleSignInClick(event) {
+    event.preventDefault();
+    signIn({ email, password });
+  }
+
+  if (signed) return <Redirect to="/dashboard" />;
+
+  return (
+    <main className="form-signin">
+      <form>
+        <img
+          className="mb-4"
+          src="/bs-logo.svg"
+          alt=""
+          width="72"
+          height="57"
+        />
+        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div className="form-floating">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="floatingPassword">Password</label>
+        </div>
+
+        <div className="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me" /> Remember me
+          </label>
+        </div>
+        <button
+          onClick={handleSignInClick}
+          className="w-100 btn btn-lg btn-primary"
+          type="submit"
+        >
+          Sign in
+        </button>
+        <p className="mt-5 mb-3 text-muted">© 2017–2021</p>
+      </form>
+    </main>
+  );
+}
+
+export default Login;
