71eb7b58b3f69b610a660934d03cda2c537dc174
Victor Turra <victor.florencio@univem.edu.br>
2021-11-28 03:22:55 -0300

    Bypass unhandled mocks

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 
 if (process.env.NODE_ENV === "development") {
   const { worker } = require("./mocks/browser");
-  worker.start();
+  worker.start({ onUnhandledRequest: "bypass" });
 }
 
 ReactDOM.render(
